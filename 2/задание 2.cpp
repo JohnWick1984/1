2ec0c163c51eb97b9923f3342fa923587bea1cf2
vboxuser <vboxuser@WINDA10>
2023-01-14 13:03:08 +0500

    Добавьте файлы проекта.

---
+++ 2/задание 2.cpp
@@ -0,0 +1,21 @@
+﻿// 2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Every" << "\n";
+	cout << "\tHunter" << "\n";
+	cout << "\t\tWants" << "\n";
+	cout << "\t\t\tTo know" << "\n";
+	cout << "\t\t\t\t Where" << "\n";
+	cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+	cout << "\t\t\t\t\t\t\tSit" << "\n\n\n\n\n";
+
+
+	return 0;
+}
+
---
+++ Home2/задание 1.cpp
@@ -0,0 +1,32 @@
+﻿// Home2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	
+	cout << "Three Days Grace \n";
+	cout << "\n";
+	cout << "Every time we lie awake\n";
+	cout << "After every hit we take\n";
+	cout << "But I haven't missed you yet\n";
+	cout << "By every sigh and  scream we make\n";
+	cout << "All the feelings that I get\n";
+	cout << "But I still don't miss you yet\n";
+	cout << "Only when I stop to think about it\n";
+	cout << "\n";
+	cout << R"(I hate everything about you
+Why do I love you?
+I hate everything about you
+Why do I love you?)";
+
+	return 0;
+}
+
+
+
+
